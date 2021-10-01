200bd61a213a2372a14ea63160ae9653b35c13c4
Samuel Corsi-House <chouse.samuel@gmail.com>
2021-10-01 07:15:55 -0400

    test(sumproductequal): fix test

--- src/math/__tests__/sumProductEqual.spec.ts
+++ src/math/__tests__/sumProductEqual.spec.ts
@@ -10,7 +10,7 @@ describe('test sumProductEqual calculator', () => {
   it('should answer with right sumProductEqual', () => {
     const result = sumProductEqual(-8, 16);
 
-    expect(result).toBe([-4, -4]);
+    expect(result).toEqual([-4, -4]);
   });
 
   it('should answer with right sumProductEqual', () => {
