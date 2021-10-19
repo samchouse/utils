5b2a489a344a8a182ba50a894fbbf54b2686d436
Samuel Corsi-House <chouse.samuel@gmail.com>
2021-10-19 17:45:08 -0400

    ci: hopefully fix build ci

---
+++ .github/scripts/decrypt.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSWORD" --output ./.github/secrets/utils_dist.p12 ./.github/secrets/utils_dist.p12.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSWORD" --output ./.github/secrets/utils.mobileprovision ./.github/secrets/utils.mobileprovision.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSWORD" --output ./.github/secrets/utils.jks ./.github/secrets/build.jks.gpg
