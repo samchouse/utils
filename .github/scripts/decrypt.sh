ea0ce80b5a877a1466d9f449faac44f38680f95a
Samuel Corsi-House <chouse.samuel@gmail.com>
2021-10-23 15:36:29 -0400

    ci: ls nodemodules

--- .github/scripts/decrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSWORD" --output ./.github/secrets/utils_dist.p12 ./.github/secrets/utils_dist.p12.gpg
-# gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSWORD" --output ./.github/secrets/utils.mobileprovision ./.github/secrets/utils.mobileprovision.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSWORD" --output ./.github/secrets/utils.jks ./.github/secrets/utils.jks.gpg
