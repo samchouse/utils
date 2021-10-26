b856af12d3942c82c51dc19fe5d413b30e1b1e06
Samuel Corsi-House <chouse.samuel@gmail.com>
2021-10-26 17:29:30 -0400

    feat: add web and api projects

---
+++ packages/api/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService]
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class AppModule {}
