3234dc9f3e799c9d3625510ebb32c3109d015f38
Samuel Corsi-House <chouse.samuel@gmail.com>
2021-11-19 07:42:35 -0500

    chore(scripts,deps): update deps and create a script to automate doing it

---
+++ scripts/updateDeps.mjs
@@ -0,0 +1,34 @@
+import ora from 'ora';
+import { run } from 'npm-check-updates';
+import path from 'path';
+
+const updateDeps = async () => {
+  let spinner = ora('Checking for updates').start();
+
+  const oldLog = console.log;
+  console.log = () => {};
+
+  const updates = await run({ cwd: process.cwd(), deep: true });
+
+  console.log = oldLog;
+
+  spinner.stop();
+
+  spinner = ora('Updating dependencies').start();
+
+  const updater = Promise.resolve(
+    Object.keys(updates).forEach(async (key) => {
+      const subUpdates = updates[key];
+      if (Object.entries(subUpdates).length > 0)
+        await run({
+          cwd: path.join(process.cwd(), path.dirname(key)),
+          upgrade: true
+        });
+    })
+  );
+
+  await updater.then(() => spinner.stop());
+  console.log('Updated all dependencies successfully');
+};
+
+updateDeps();
